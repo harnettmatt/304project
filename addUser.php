b592d65438fe31d9f772e7a0e6b501ccaa3935f0
Bhawandeep <bhawandeep.kambo@gmail.com>
2015-06-14 09:33:07 -0700

    addusers

---
+++ addUser.php
@@ -0,0 +1,53 @@
+
+<html>
+<head>
+<title>Add User</title>
+</head>
+<body>
+<form action="http://localhost:63342/304project/UserAdded.php" method="post">
+
+<b>Add a New User</b>
+
+<p>First Name:
+<input type="text" name="FirstName" size="30" value="" />
+</p>
+
+<p>Last Name:
+<input type="text" name="LastName" size="30" value="" />
+</p>
+
+<p>Phone:
+<input type="text" name="Phone" size="30" value="" />
+</p>
+
+<p>Address:
+<input type="text" name="Address" size="30" value="" />
+</p>
+
+<p>User Name:
+<input type="text" name="UserName" size="30" value="" />
+</p>
+
+<p>Password:
+<input type="text" name="Password" size="30" value="" />
+</p>
+
+<p>Credit Card:
+<input type="text" name="CreditCard" size="30" value="" />
+</p>
+
+<p>Email:
+<input type="text" name="Email" size="30" value="" />
+</p>
+
+<p>Account Type:
+<input type="text" name="AccountType" size="30" value="" />
+</p>
+
+<p>
+<input type="submit" name="submit" value="Send" />
+</p>
+
+</form>
+</body>
+</html>
